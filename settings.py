0a7b2aa41007669bd8eac4e09f924968ea5dc663
GALL965 <bonaisdecamote45@gmail.com>
2025-06-19 09:10:31 -0600

    w

--- settings.py
+++ settings.py
@@ -1,1 +1,15 @@
-ALLOWED_HOSTS = ['*']  # temporalmente para pruebas, luego lo puedes restringir
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Cargar las variables del archivo .env
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
